1284a6558accea27b97586e888b7139bab2548b3
Ben Corlett <bencorlett@me.com>
2011-12-14 10:24:30 +1100

    Structuring class better.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- classes/geocode.php
+++ classes/geocode.php
@@ -4,56 +4,79 @@ namespace Geocode;
 
 class Geocode
 {
-	const SERVICE_URL = 'http://maps.googleapis.com/maps/api/geocode/';
+	/**
+	 * The service url for geocoding information
+	 * 
+	 * @var string
+	 */
+	protected $service_url = 'http://amaps.googleapis.com/maps/api/geocode/json';
 
-	protected $format = 'xml';
+	/**
+	 * The type of geocoding we are doing,
+	 * normal (address) or reverse (lat & long)
+	 * 
+	 * @var string
+	 */
+	public $type = 'address';
 
-	protected $sensor = true;
+	/**
+	 * The parameters used in
+	 * the request
+	 */
+	public $params = array();
 
-	protected $type;
-
-	protected $params;
-
-	public function __construct()
+	protected function __construct($type, $params = array())
 	{
-		$args = func_get_args();
+		\Config::load('geocode', true);
 
-		$this->type = array_shift($args);
-
-		$this->params = $args;
+		$this->params = \Arr::merge($this->params, \Config::get('geocode.default_params', array()), $params);
 	}
 
 	public static function address($address)
 	{
-		return new static('address', $address);
+		return new static('address', array(
+			'address' => $address,
+		));
 	}
 
 	public static function reverse($lat, $long)
 	{
-		return new static('reverse', $lat, $long);
+		return new static('reverse', array(
+			'latlng' => $lat.','.$lng,
+		));
 	}
 
 	public function execute()
 	{
 		$url = $this->build_url();
 
-		echo $url;
+		try
+		{
+			$request = \Request::forge($url, array(
+				'driver' => 'curl',
+			));
+			$request->set_option('timeout', 3);
+
+			$body = $request->execute()->response()->body();
+
+			\Debug::dump($body);
+		}
+		catch (\RequestException $e)
+		{
+			if ($request->response_info('http_code', false) == '404')
+			{
+				throw new \Exception('The geocode resource could not be found (404)');
+			}
+
+			throw new \Exception('Curl request exception: '.
+				$message = $e->getMessage() ? '"'.$message.'"' : 'No message specified.');
+		}
 	}
 
 	public function build_url()
 	{
-		if ($this->type = 'address')
-		{
-			$params = array('address' => $this->params[0]);
-		}
-		else
-		{
-			$params = array('latlng' => implode(',', $this->params));
-		}
 
-		$params['sensor'] = (bool) $this->sensor ? 'true' : 'false';
-
-		$url = self::SERVICE_URL.$this->format.'?'.http_build_query($params, null, '&');
+		$url = $this->service_url.'?'.http_build_query($this->params, null, '&');
 
 		return $url;
 	}
---
+++ config/geocode.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+	/**
+	 * Default parameters for
+	 * requests
+	 */
+	'default_params' => array(
+		'sensor' => 'true',
+		'lang'   => 'en',
+	),	
+);
